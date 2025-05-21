cd60a32259b662d22c63ba4c6697cccfdbfabbfe
thekukieff <i.kukieff@gmail.com>
2025-05-21 13:27:34 +0500

    asd

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -18,17 +18,47 @@ namespace WpfApp4
     public partial class MainWindow : Window
     {
         
-
+        public ObservableCollection<ToDo> Todo = new ObservableCollection<ToDo>();
 
         public MainWindow()
         {
             InitializeComponent();
-            
+            descriptionToDo.Text = "Описания нет";
+            dateToDo.SelectedDate = new DateTime(2024, 1, 10);
+
+            Todo.Add(new ToDo("Приготовить покушать", new DateTime(2024, 1, 15), "Описания нет"));
+            Todo.Add(new ToDo("Поработать", new DateTime(2024, 1, 20), "Сьездить на совещения в Москву"));
+            Todo.Add(new ToDo("Отдохнуть", new DateTime(2024, 1, 1), "Сьездить в отпуску в Сочи"));
+            listToDo.ItemsSource = Todo;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void CheckBox_Cheked(object sender, RoutedEventArgs e)
         {
-            titleToDo2.Text = titleToDo1.text; 
+            if (groupBoxToDo?.Visibility != null)
+            {
+                groupBoxToDo.Visibility = Visibility.Visible;
+            }
         }
+
+        private void CheckBox_Uncheked(object sender, RoutedEventArgs e)
+        {
+            groupBoxToDo.Visibility = Visibility.Hidden;
+        }
+
+        private void CreateToDo(object sender, RoutedEventArgs e)
+        {
+            var temp = dateToDo.SelectedDate.Value;
+            Todo.Add(new ToDo(titleToDo.Text, new DateTime(temp.Year, temp.Month, temp.Day), descriptionToDo.Text));
+        }
+
+        private void DeleteToDo(object sender, RoutedEventArgs e)
+        {
+            if (listToDo.SelectedItem as ToDo == null) { return; }
+            else
+            {
+                Todo.Remove(listToDo.SelectedItem as ToDo);
+            }
+        }
+
     }
 }
--- WpfApp4/obj/Debug/net8.0-windows/App.g.i.cs
+++ WpfApp4/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace WpfApp4 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace WpfApp4 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public static void Main() {
             WpfApp4.App app = new WpfApp4.App();
             app.InitializeComponent();
